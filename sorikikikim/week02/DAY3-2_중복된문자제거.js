c47b0773e6e17491d3a94b56926279cedee9b0aa
sorikikikim <sorikim98@naver.com>
2023-08-17 16:41:13 +0900

    week2 DAY3

---
+++ sorikikikim/week02/DAY3-2_중복된문자제거.js
@@ -0,0 +1,4 @@
+const solution = (my_string) => {
+    let set = new Set(my_string);
+    return [...set].join('');
+};
---
+++ sorikikikim/week02/DAY3-3_A로B만들기.js
@@ -0,0 +1,4 @@
+// 어려워요....
+const solution = (before, after) => {
+	return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0
+}
