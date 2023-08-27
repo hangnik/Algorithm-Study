2cc6a4b81a130d0a61b151f454f97207282b1cb1
sorikikikim <sorikim98@naver.com>
2023-08-27 23:53:35 +0900

    week3 DAY3

---
+++ sorikikikim/week03/DAY3-3_저주의숫자3.js
@@ -0,0 +1,10 @@
+const solution = n => {
+	let result = 0;
+	for (let i = 0; i < n; i++) {
+		result++;
+		while (result % 3 === 0 || result.toString().includes('3')) {
+			result++;
+		}
+	}
+	return result;
+}
