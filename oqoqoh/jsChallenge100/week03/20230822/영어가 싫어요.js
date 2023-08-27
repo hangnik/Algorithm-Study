89a1c8fea93f3dd621f6a140659b271a59518d46
oqoqoh <oqoq59@gmail.com>
2023-08-27 22:29:49 +0900

    week03

---
+++ oqoqoh/jsChallenge100/week03/20230822/영어가 싫어요.js
@@ -0,0 +1,8 @@
+function solution(numbers) {}
+
+const num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+`
+"onetwothreefourfivesixseveneightnine"	123456789
+"onefourzerosixseven"	14067
+`;
